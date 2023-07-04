66d14a462094e7dd63040951118a082e5684be51
Ben Kersey <kersey24@ad.unc.edu>
2023-07-04 17:28:01 -0400

    skeleton

---
+++ library.js
@@ -0,0 +1,11 @@
+let myLibrary = [];
+
+function Book(title, author, read) {
+  this.title = title
+  this.author = author
+  this.read = read
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book)
+}
